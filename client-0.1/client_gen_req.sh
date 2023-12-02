84832ce6edb9c2f8126ce0b84c8832a55c98e743
Dokukin Vyacheslav Olegovich <toorrp4@gmail.com>
2023-12-03 00:44:12 +0300

    Добавил в postinst запрос email у пользователя, для замены по шаблону в файле конфигурации easy-rsa vars, а также добавил команды в файл генерации запроса на подпись

--- client-0.1/client_gen_req.sh
+++ client-0.1/client_gen_req.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+PATHEASYRSAUSR="/opt/easy-rsa"
+LOGIN=$USER
+
+cd ${PATHEASYRSAUSR}
+
+read "Enter your user login [${USER}]: " login
+if [ ! -z $login ]; then
+    LOGIN=$login
+fi
+
+./easyrsa gen-req ${LOGIN} nopass
+
