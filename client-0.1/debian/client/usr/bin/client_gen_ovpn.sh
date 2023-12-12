4b58ea4407f933a54535d1eb3c348381d1c73d9c
Dokukin Vyacheslav Olegovich <toorrp4@gmail.com>
2023-12-13 00:56:24 +0300

    Убрал добавление группы, т.к она по умолчанию есть в Ubuntu 20.04, это приводило к ошибке groupadd: group 'nogroup' already exists

--- client-0.1/debian/client/usr/bin/client_gen_ovpn.sh
+++ client-0.1/debian/client/usr/bin/client_gen_ovpn.sh
@@ -2,51 +2,55 @@
 
 # check argument (client login)
 if [ -z "$1" ]; then
-  echo "Error: $0 CLIENT_NAME - break!"
+  echo -e "\033[31m Error: $0 CLIENT_NAME - break! \033[0m"
   exit 1
 fi
 
 CLIENT_NAME="$1"
-PATHEASYRSAUSR="/opt/easy-rsa"
-PATHOVPNUSR="~/ovpn"
-PATHOVPNCLIENT="/var/snap/simple-openvpn-client/common/"
 
-sudo mkdir -p ${PATHOVPNUSR}/$CLIENT_NAME
-sudo cp ${PATHOVPNUSR}/base.conf ${PATHOVPNUSR}/$CLIENT_NAME/client.ovpn
-sudo chown -R $USER:$USER ${PATHOVPNUSR}/$CLIENT_NAME/client.ovpn
-sudo chmod 766 ${PATHOVPNUSR}/$CLIENT_NAME/client.ovpn
-cd $PATHEASYRSAUSR
-source ./vars
-./easyrsa build-key $CLIENT_NAME
-#(In the new version ./easyrsa build-client-full  $CLIENT_NAME)
+#!/bin/bash
+# First argument: Client identifier
+KEY_DIR=/opt/easy-rsa/pki/private
+OUTPUT_DIR=~/ovpn
+BASE_CONFIG=/etc/openvpn/client/base.conf
 
-# Copy the client keys and certificates to the client directory
-sudo cp ${$PATHEASYRSAUSR}/pki/issued/$CLIENT_NAME.crt ${PATHOVPNUSR}/$CLIENT_NAME/
-sudo cp ${$PATHEASYRSAUSR}/pki/private/$CLIENT_NAME.key ${PATHOVPNUSR}/$CLIENT_NAME/
-sudo cp ${PATHOVPNUSR}/keys/ca.crt ${PATHOVPNUSR}/$CLIENT_NAME/
-sudo cp ${PATHOVPNUSR}/keys/ta.key ${PATHOVPNUSR}/$CLIENT_NAME/
+# change user and permission
+sudo chown -R $USER:$USER "/opt/easy-rsa"
 
-sudo echo "<tls-crypt>" >>${PATHOVPNUSR}/$CLIENT_NAME/ta.key >> ${PATHOVPNUSR}/$CLIENT_NAME/client.ovpn
-sudo cat ${PATHOVPNUSR}/$CLIENT_NAME/ta.key >> ${PATHOVPNUSR}/$CLIENT_NAME/client.ovpn
-sudo echo "</tls-crypt>
-
-<ca>" >> ${PATHOVPNUSR}/$CLIENT_NAME/client.ovpn
-sudo cat ${PATHOVPNUSR}/$CLIENT_NAME/ca.crt  >> ${PATHOVPNUSR}/$CLIENT_NAME/client.ovpn
-sudo echo "</ca>
-
-<cert>" >> ${PATHOVPNUSR}/$CLIENT_NAME/client.ovpn
-sudo cat ${PATHOVPNUSR}/$CLIENT_NAME/$CLIENT_NAME.crt >> ${PATHOVPNUSR}/$CLIENT_NAME/client.ovpn
-sudo echo "</cert>
-
-<key>" >> ${PATHOVPNUSR}/$CLIENT_NAME/client.ovpn
-sudo cat ${PATHOVPNUSR}/$CLIENT_NAME/$CLIENT_NAME.key  >> ${PATHOVPNUSR}/$CLIENT_NAME/client.ovpn
-sudo echo "</key>" >> ${PATHOVPNUSR}/$CLIENT_NAME/client.ovpn
-
-sudo echo "redirect-gateway def1" >> ${PATHOVPNUSR}/$CLIENT_NAME/client.ovpn
-
-# copy config to simple-openvpn-client folder
-if [ -d $PATHOVPNCLIENT ]; then
-    cp ${PATHOVPNUSR}/$CLIENT_NAME/client.ovpn $PATHOVPNCLIENT
-else
-    echo "Error simple-openvpn-client folder is not exist!"
+if [ ! -f ${KEY_DIR}/${CLIENT_NAME}.crt ]; then
+  echo -e "\033[31m Error: file ${KEY_DIR}/${CLIENT_NAME}.crt not exist! \033[0m"
+  exit 1
 fi
+
+if [ ! -f $KEY_DIR/ca.crt ]; then
+  echo -e "\033[31m Error: file ${BASE_CONFIG}/ta.key not exist! \033[0m"
+  exit 1
+fi
+
+if [ ! -f $KEY_DIR/ta.key ]; then
+  echo -e "\033[31m Error: file ${BASE_CONFIG}/ta.key not exist! \033[0m"
+  exit 1
+fi
+
+if [ ! -f $BASE_CONFIG ]; then
+  echo -e "\033[31m Error: file ${BASE_CONFIG} not exist! \033[0m"
+  exit 1
+fi
+
+if [ ! -d $OUTPUT_DIR ]; then
+  mkdir -p $OUTPUT_DIR
+  sudo chmod 700 $OUTPUT_DIR
+fi
+
+cat ${BASE_CONFIG} \
+<(echo -e '<ca>') \
+${KEY_DIR}/ca.crt \
+<(echo -e '</ca>\n<cert>') \
+${KEY_DIR}/${CLIENT_NAME}.crt \
+<(echo -e '</cert>\n<key>') \
+${KEY_DIR}/${CLIENT_NAME}.key \
+<(echo -e '</key>\n<tls-crypt>') \
+${KEY_DIR}/ta.key \
+<(echo -e '</tls-crypt>\n') \
+> ${OUTPUT_DIR}/${CLIENT_NAME}.ovpn
+#echo "redirect-gateway def1" >> ${OUTPUT_DIR}/${CLIENT_NAME}.ovpn
--- client-0.1/debian/client/usr/bin/client_gen_req.sh
+++ client-0.1/debian/client/usr/bin/client_gen_req.sh
@@ -4,7 +4,7 @@ PATHEASYRSAUSR="/opt/easy-rsa"
 
 # check argument (client login)
 if [ -z "$1" ]; then
-  echo "Error: $0 CLIENT_NAME - break!"
+  echo "\033[31m Error: $0 CLIENT_NAME - break! \033[0m"
   exit 1
 fi
 
