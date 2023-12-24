f5fe8b4dc89a529e61ba4443ca02a5ae31505fcd
Dokukin Vyacheslav Olegovich <toorrp4@gmail.com>
2023-12-24 15:27:11 +0300

    Добавил файл конфига .s3cnf и удаление конфигов пакета из /etc

--- push.sh
+++ push.sh
@@ -42,4 +42,4 @@ scp pki_0.1-1_all.deb ovpn_0.1-1_all.deb bcp_0.1-1_all.deb ${LOGIN}@${HOST}:/hom
 
 # connet to remote server
 # ./bcp_0.1-1_all.deb
-ssh ${LOGIN}@${HOST} -t "sudo apt update && sudo apt full-upgrade; sudo apt remove pki ovpn bcp easy-rsa iptables-persistent net-tools python3-pip prometheus-node-exporter gnupg; sudo apt autoremove; sudo apt install ./pki_0.1-1_all.deb ./ovpn_0.1-1_all.deb ./bcp_0.1-1_all.deb; exit"
+ssh ${LOGIN}@${HOST} -t "sudo apt update && sudo apt full-upgrade; sudo apt remove pki ovpn bcp easy-rsa iptables-persistent net-tools prometheus-node-exporter gnupg s3cmd; sudo apt autoremove; sudo apt install ./pki_0.1-1_all.deb ./ovpn_0.1-1_all.deb; exit"
