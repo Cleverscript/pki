89f39fe338f89f9c28bca8fa7745c17ef9b48628
Dokukin Vyacheslav Olegovich <toorrp4@gmail.com>
2023-12-27 01:21:12 +0300

    Исправлены ошибки записи в лог файл, добавлен третий параметр при вызове скрипта из Unit сервиса, в котором должен быть передан логин пользователя

--- bac-0.1/bac.sh
+++ bac-0.1/bac.sh
@@ -7,16 +7,35 @@
 # Category   :  Desktop
 ##########################################################################
 
-BACDATETIME=$(date +"%d.%m.%Y_%H-%M-%S")
+BACDATETIME=$(date +"%d.%m.%Y %H:%M:%S")
 BACCNF=/etc/bac.conf
 BACGPG="$1"
 BACCONTAINER="$2"
+BACUSER="$3"
 BACNAME=bac_$(date "+%d%m%Y")
 BACLOG=/var/log/bac.log
 
 # check config
 if [ ! -f $BACCNF ]; then
     sudo bash -c "echo '[${BACDATETIME}] Error: config file ${BACCNF} is not exist!' >> '${BACLOG}';"
+    exit 1
+fi
+
+# check GPG key
+if [ -z $BACGPG ]; then
+    sudo bash -c "echo '[${BACDATETIME}] Error: PGP key is null, exited!' >> '${BACLOG}';"
+    exit 1
+fi
+
+# check SELECTEL storege container name
+if [ -z $BACCONTAINER ]; then
+    sudo bash -c "echo '[${BACDATETIME}] Error: SELECTEL storege container name is null, exited!' >> '${BACLOG}';"
+    exit 1
+fi
+
+# check user
+if [ -z $BACUSER ]; then
+    sudo bash -c "echo '[${BACDATETIME}] Error: user is null, exited!' >> '${BACLOG}';"
     exit 1
 fi
 
@@ -41,26 +60,41 @@ fi
 
 cd /tmp
 
+# Packing into array
 while read line
 do
-    # Packing into an array
     if [ ! -z "$line" ]; then
-        cp -r "$line" /tmp/${BACNAME}/ >/dev/null
+        sudo bash -c "'cp -r $line /tmp/${BACNAME}/' > '/dev/null';"
     fi
-    tar -zcf ${BACNAME}.tar.gz /tmp/${BACNAME} >/dev/null
+    sudo bash -c "tar -zcf ${BACNAME}.tar.gz /tmp/${BACNAME} > '/dev/null';"
+    if [ ! -f /tmp/${BACNAME}.tar.gz ]; then
+        sudo bash -c "echo '[${BACDATETIME}] Error: create backup file ${BACNAME}.tar.gz, exited!' >> '${BACLOG}';"
+    fi
     sudo bash -c "echo '[${BACDATETIME}] Success: create backup file ${BACNAME}.tar.gz!' >> '${BACLOG}';"
-    sudo bash -c "'tar -tf ${BACNAME}.tar.gz' >> '${BACLOG}';"
+    sudo bash -c "chown $BACUSER:$BACUSER /tmp/${BACNAME}.tar.gz"
+    sudo bash -c "ls -la /tmp/${BACNAME}.tar.gz >> '${BACLOG}';"
+    sudo bash -c "tar -tf ${BACNAME}.tar.gz >> '${BACLOG}';"
+
 done < $BACCNF
 
 # GPG encript file array
 if [ ! -z $BACGPG ]; then
-
-    gpg --encrypt --sign --armor -r ${BACGPG} /tmp/${BACNAME}.tar.gz
+    USR=$USER
+    sudo bash -c "echo 'GPG enc start: use key <${BACGPG}>, user run $USR $BACUSER' >> '${BACLOG}';"
+    gpg --sign-key ${BACGPG}
+    #gpg --no-tty --encrypt --sign --armor -r ${BACGPG} /tmp/${BACNAME}.tar.gz
+    # --detach-sign --batch --no-tty --yes
+    GPG_TTY=$(tty)
+    export GPG_TTY
+    #sudo -H -u $BACUSER bash -c "gpg --encrypt --no-tty --yes --sign --armor -r ${BACGPG} /tmp/${BACNAME}.tar.gz"
+    gpg --encrypt --no-tty --yes --sign --armor -r ${BACGPG} /tmp/${BACNAME}.tar.gz
+    GPGLISTKEY=$(sudo -H -u $BACUSER bash -c "gpg --list-key")
+    sudo bash -c "echo '$GPGLISTKEY' >> '${BACLOG}';"
 
     if [ -f /tmp/${BACNAME}.tar.gz.asc ]; then
-        rm -f /tmp/${BACNAME}.tar.gz && sudo bash -c "echo '[${BACDATETIME}] Remove /tmp/${BACNAME}.tar.gz' >> '${BACLOG}';"
+        sudo bash -c "rm -f /tmp/${BACNAME}.tar.gz" && sudo bash -c "echo '[${BACDATETIME}] Remove /tmp/${BACNAME}.tar.gz' >> '${BACLOG}';"
         sudo bash -c "echo '[${BACDATETIME}] Success GPG encrypt file array, new file:' >> '${BACLOG}';"
-        ls -la /tmp/${BACNAME}.tar.gz.asc >> ${BACLOG}
+        sudo bash -c "ls -la /tmp/${BACNAME}.tar.gz.asc >> '${BACLOG}';"
     else
         sudo bash -c "echo '[${BACDATETIME}] Error GPG encrypt file array fail' >> '${BACLOG}';"
         exit 1
