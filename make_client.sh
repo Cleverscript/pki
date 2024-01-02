68a22fda8e5f22f007f0b4a65075339fb479e606
Dokukin Vyacheslav Olegovich <toorrp4@gmail.com>
2024-01-02 13:36:22 +0300

    Доработал скрипт автоматизации пересборке пакета client

--- make_client.sh
+++ make_client.sh
@@ -4,15 +4,9 @@ LOGIN=$1
 HOST=$2
 
 # remove .deb package
-rm -f pki_0.1.* && rm -f ovpn_0.1-1_* && rm -f client_0.1-1_*
+rm -f client_0.1-1_*
 
-# generate .deb packets
-cd pki-0.1/debian && debuild -b
-cd ../../
-cd ovpn-0.1/debian && debuild -b
-cd ../../
-cd bcp-0.1/debian && debuild -b
-cd ../../
+# debuild .deb package
 cd client-0.1/debian && debuild -b
 cd ../../
 
@@ -37,27 +31,28 @@ case "$UPLOAD" in
             echo -e "\033[31m Error - empty login remote host -break! \033[0m"
             exit 1
         fi
-
         if [ -z $HOST ]; then
             echo -e "\033[31m Error - empty IP or domain remote host -break! \033[0m"
             exit 1
         fi
 
         # push new deb packets to remote server
-        scp pki_0.1-1_all.deb ovpn_0.1-1_all.deb ${LOGIN}@${HOST}:/home/${LOGIN}
+        scp client_0.1-1_all.deb ${LOGIN}@${HOST}:/home/${LOGIN}
 
-        read -p "Install packages to the server? [y/n]: " INSTALL
+        read -p "Install package to the server? [y/n]: " INSTALL
 
         case "$INSTALL" in
             y)
                 # connet to remote server
-                ssh ${LOGIN}@${HOST} -t "sudo apt update && sudo apt full-upgrade; sudo apt remove pki ovpn bcp easy-rsa iptables-persistent net-tools prometheus-node-exporter gpg s3cmd; sudo apt autoremove; sudo apt install ./pki_0.1-1_all.deb ./ovpn_0.1-1_all.deb; exit"
+                ssh ${LOGIN}@${HOST} -t "sudo apt remove gpg easy-rsa openvpn client; sudo apt autoremove; sudo apt install ./client_0.1-1_all.deb; exit"
             ;;
+
             *)
                 echo "Exited!"
                 exit 0
             ;;
         esac
+
     ;;
 
     *)
